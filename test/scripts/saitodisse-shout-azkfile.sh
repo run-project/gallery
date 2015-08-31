4f096f7331970f24d04f7b1a47b9e24dafdaf7fa
Julio Makdisse Saito <saitodisse@gmail.com>
2015-08-31 15:53:46 -0300

    do not stop after start

--- test/scripts/saitodisse-shout-azkfile.sh
+++ test/scripts/saitodisse-shout-azkfile.sh
@@ -132,3 +132,51 @@ echo " +++++++++++++++++++++++++++"
 echo "  restarting with reprovision..."
 echo " +++++++++++++++++++++++++++"
 azk start -Rovv saitodisse/shout#azkfile /tmp/buttons/shout
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo " saitodisse/shout#azkfile"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+
+echo ""
+echo "cd /tmp/buttons/shout folder"
+cd /tmp/buttons/shout
+
+echo ""
+echo ""
+echo " +++++++++++++++++++++++++++"
+echo "  $ azk stop"
+echo " +++++++++++++++++++++++++++"
+azk stop
+
+echo ""
+echo ""
+echo " +++++++++++++++++++++++++++"
+echo "  removing any persistent/sync folders..."
+echo " +++++++++++++++++++++++++++"
+azk info |
+  grep -e "(sync_folders|persistent_folders)" |
+  awk -F ":" "{ print $2 }" |
+  sed "s/.*(persistent_folders|sync_folders)/(w+).*/\2/g" |
+  tail -n 1 |
+  xargs -n 1 -I VARR sudo rm -rf ~/.azk/data/sync_folders/VARR ~/.azk/data/persistent_folders/VARR
+
+echo ""
+echo ""
+echo " +++++++++++++++++++++++++++"
+echo "  removing old project folder if exists..."
+echo " +++++++++++++++++++++++++++"
+cd /tmp/buttons
+sudo rm -rf /tmp/buttons/shout
+
+echo ""
+echo ""
+echo " +++++++++++++++++++++++++++"
+echo "  restarting with reprovision..."
+echo " +++++++++++++++++++++++++++"
+azk start -Rovv saitodisse/shout#azkfile /tmp/buttons/shout
