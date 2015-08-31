6233d4773ad5374aa0e48d88a38655cab3abc7fa
Julio Makdisse Saito <saitodisse@gmail.com>
2015-08-31 15:20:34 -0300

    start script does not clear after start

--- test/scripts/saitodisse-dillinger-azkfile.sh
+++ test/scripts/saitodisse-dillinger-azkfile.sh
@@ -84,3 +84,51 @@ cd /tmp/buttons
 sudo rm -rf /tmp/buttons/dillinger
 
 
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo " saitodisse/dillinger#azkfile"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+
+echo ""
+echo "cd /tmp/buttons/dillinger folder"
+cd /tmp/buttons/dillinger
+
+echo ""
+echo ""
+echo " +++++++++++++++++++++++++++"
+echo "  $ azk stop"
+echo " +++++++++++++++++++++++++++"
+azk stop
+
+echo ""
+echo ""
+echo " +++++++++++++++++++++++++++"
+echo "  removing any persistent/sync folders..."
+echo " +++++++++++++++++++++++++++"
+azk info |
+  grep -e "(sync_folders|persistent_folders)" |
+  awk -F ":" "{ print $2 }" |
+  sed "s/.*(persistent_folders|sync_folders)/(w+).*/\2/g" |
+  tail -n 1 |
+  xargs -n 1 -I VARR sudo rm -rf ~/.azk/data/sync_folders/VARR ~/.azk/data/persistent_folders/VARR
+
+echo ""
+echo ""
+echo " +++++++++++++++++++++++++++"
+echo "  removing old project folder if exists..."
+echo " +++++++++++++++++++++++++++"
+cd /tmp/buttons
+sudo rm -rf /tmp/buttons/dillinger
+
+echo ""
+echo ""
+echo " +++++++++++++++++++++++++++"
+echo "  restarting with reprovision..."
+echo " +++++++++++++++++++++++++++"
+azk start -Rovv saitodisse/dillinger#azkfile /tmp/buttons/dillinger
